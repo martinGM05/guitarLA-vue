0dbb49fc36f00857687013dcc51599da94ccfe72
Martin <mgonzalez@mediomelon.mx>
2023-11-16 01:47:27 -0600

    Upload respository

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+    <Header
+        :carrito="carrito"
+        :guitarra="guitarra"
+        @decrementar-cantidad="decrementarCantidad"
+        @incrementar-cantidad="incrementarCantidad"
+        @agregar-carrito="agregarCarrito"
+        @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
+    />
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+            <Guitarra 
+                v-for="guitarra in guitarras"
+                :key="guitarra.id"
+                :guitarra="guitarra"
+                @agregar-carrito="agregarCarrito"
+            />
+        </div>
+    </main>
+    <Footer />
+
+
+</template>
+
+<script setup>
+    import {ref, onMounted, watch} from 'vue';
+    import { db } from './data/guitarras'
+    import Guitarra from './components/Guitarra.vue'
+    import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
+
+    const guitarras = ref([])
+    const carrito = ref([])
+    const guitarra = ref({})
+    
+    watch(carrito, () => {
+        guardarLocalStorage();
+    }, {
+        deep: true,
+    })
+
+    onMounted(() => {
+        guitarras.value = db;
+        guitarra.value = db[3];
+
+        const carritoStorage = localStorage.getItem('carrito')
+        if(carritoStorage){
+            carrito.value = JSON.parse(carritoStorage)
+        }
+    })
+
+    const guardarLocalStorage = () => {
+        localStorage.setItem('carrito', JSON.stringify(carrito.value))
+    }
+
+    const agregarCarrito = (guitarra) => {
+        const existCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+        if(existCarrito >= 0) {
+            carrito.value[existCarrito].cantidad++;
+        } else {
+            guitarra.cantidad = 1
+            carrito.value.push(guitarra)
+        }
+    }
+
+    const decrementarCantidad = (id) => {
+        const index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad <= 1) return;
+        carrito.value[index].cantidad--
+    }
+
+    const incrementarCantidad = (id) => {
+        const index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad >= 5) return;
+        carrito.value[index].cantidad++
+    }
+
+    const eliminarProducto = (id) => {
+        carrito.value = carrito.value.filter(guitarra => guitarra.id != id)
+    }
+
+    const vaciarCarrito = () => {
+        carrito.value = []
+    }
+    
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
